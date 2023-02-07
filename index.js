8b9c2a1b246c6ad7f35530f6ea7e6c70579861aa
thuy <109534863+Thuy2y2c@users.noreply.github.com>
2023-02-07 13:23:05 +0700

    bro is anticheat friendly

--- index.js
+++ index.js
@@ -4,9 +4,6 @@ const ms = require('ms');
 
 console.log('\x1b[33m%s\x1b[0m','[Console] Creating bot...');
 
-    // Mineflayer plugins :
-    var tpsPlugin = require('mineflayer-tps')(mineflayer);
-    
 const botArgs = { // Create bot
     host: '51.81.220.187', // 51.81.220.187 for 8b8t
     port: '25565',
@@ -19,9 +16,6 @@ const initBot = () => {
     // Create the bot
     let bot = mineflayer.createBot(botArgs);
 
-    // Load Mineflayer plugins :
-    bot.loadPlugin(tpsPlugin) // tps
-
     console.log('\x1b[33m%s\x1b[0m',`[Console] Logged in as ${username}`);
 
     bot.on('message', message => { // logs messages to console + logins for the server
@@ -32,21 +26,21 @@ const initBot = () => {
                 console.log('\x1b[33m%s\x1b[0m','[Console] Bot has joined the server!') } // Bot has joined the server
       });
     
-      bot.on("move", ()=>{ // THE FRIENDLY FEATURE. (remove if you want) https://streamable.com/2pj3ur
-        let gamer = bot.nearestEntity();
+      bot.on("move", ()=>{ // Tried this on 2b2t. A player kidnapped the bot.
+        let enemy = bot.nearestEntity();
     
-        if (gamer) {
-            bot.lookAt(gamer.position.offset(0, gamer.height, 0)); // look at their head
-            bot.setControlState('jump', true) 
+        if (enemy) {
+            bot.lookAt(enemy.position.offset(0, enemy.height, 0))
             bot.swingArm('right')
             setInterval(() => {
-                bot.setControlState('sneak', true) 
-                bot.setControlState('sneak', false);
-              }, 450); // the lower the amount the friendlier (this could lags the server remove if u want) 
-         // console.log(`looking at ${gamer.username}`)
-        }
+               bot.setControlState('sneak', true)
+           }, 450);
+           setInterval(() => {
+            bot.setControlState('sneak', false)
+        }, 200); // friendly got cool downs for anticheats
+      }
     });
-
+    
     bot.on('death', () => {
         console.log('\x1b[33m%s\x1b[0m',
            `Bot killed.. respawning..`
